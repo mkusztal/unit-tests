3ddcdd1215bbf4d72d45e2bebbeeb975c9b3e947
Maciej Kusztal <maciej.kusztal1995@gmail.com>
2022-05-31 20:04:10 +0300

    Set values to fields

---
+++ src/components/CurrencyForm/CurrencyForm.test.js
@@ -0,0 +1,34 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import CurrencyForm from './CurrencyForm';
+
+describe('Component CurrencyForm', () => {
+  it('should render without crashing', () => {
+    render(<CurrencyForm action={() => {}} />);
+  });
+  it('should run action callback with proper data on form submit', () => {
+    const action = jest.fn();
+
+    // render component
+    render(<CurrencyForm action={action} />);
+
+    // find "convert" button
+    const submitButton = screen.getByText('Convert');
+
+    // simulate user click on "convert" button
+    userEvent.click(submitButton);
+
+    //check if action callback was called once
+    expect(action).toHaveBeenCalledTimes(1);
+
+    // find field elems
+    const amountField = screen.getByTestId('amount');
+    const fromField = screen.getByTestId('from-select');
+    const field = screen.getByTestId('to-select');
+
+    // set test values to fields
+    userEvent.type(amountField, 'Lorem Ipsum');
+    userEvent.selectOptions(fromField, 'PLN');
+    userEvent.selectOptions(field, 'USD');
+  });
+});
